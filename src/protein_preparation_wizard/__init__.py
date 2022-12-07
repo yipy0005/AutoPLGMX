c3215ebf3289af0cb46b57a3f8929efcda418803
yipy0005 <yipy0005@gmail.com>
2022-12-07 16:52:54 +0800

    add documentation to the download_pdb function

--- src/protein_preparation_wizard/__init__.py
+++ src/protein_preparation_wizard/__init__.py
@@ -3,6 +3,16 @@ import requests
 
 
 def download_pdb(pdb_code: str) -> Optional[List[str]]:
+    """
+    Download a PDB file from the web.
+
+    Args:
+        pdb_code (str): The 4-letter PDB ID
+
+    Returns:
+        Optional[List[str]]: Contents of the PDB file as a list of strings, otherwise None
+    """
+    
     pdb_url: str = f"http://www.rcsb.org/pdb/files/{pdb_code}.pdb"
 
     response = requests.get(pdb_url)
